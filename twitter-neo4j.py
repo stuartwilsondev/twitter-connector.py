4a6dcb3cd1066f967bb87331841deb6f5039ce95
Stuart Wilson <stuart@stuartwilsondev.com>
2015-09-30 16:20:21 +0100

    Attempt some error handling

--- twitter-neo4j.py
+++ twitter-neo4j.py
@@ -5,6 +5,10 @@ import ConfigParser
 import json
 from optparse import OptionParser
 from py2neo import Graph, authenticate
+from httplib import IncompleteRead
+from requests.packages.urllib3.exceptions import ProtocolError
+import time
+
 
 authenticate("localhost:7474", "neo4j", "pa55word")
 graph = Graph()
@@ -38,61 +42,64 @@ class listener(StreamListener):
         #Load Json
         tweet = json.loads(data)
        
+        if all (key in tweet for key in ("id","text","created_at","user")):
 
-        query = """
-        UNWIND {tweet} AS t
-        WITH t
-        ORDER BY t.id
-        WITH t,
-             t.entities AS e,
-             t.user AS u,
-             t.retweeted_status AS retweet
-        MERGE (tweet:Tweet {id:t.id})
-        SET tweet.text = t.text,
-            tweet.created_at = t.created_at,
-            tweet.favorites = t.favorite_count
-        MERGE (user:User {screen_name:u.screen_name})
-        SET user.name = u.name,
-            user.location = u.location,
-            user.followers = u.followers_count,
-            user.following = u.friends_count,
-            user.statuses = u.statusus_count,
-            user.profile_image_url = u.profile_image_url
-        MERGE (user)-[:POSTS]->(tweet)
-        MERGE (source:Source {name:t.source})
-        MERGE (tweet)-[:USING]->(source)
-        FOREACH (h IN e.hashtags |
-          MERGE (tag:Hashtag {name:LOWER(h.text)})
-          MERGE (tag)-[:TAGS]->(tweet)
-        )
-        FOREACH (u IN e.urls |
-          MERGE (url:Link {url:u.expanded_url})
-          MERGE (tweet)-[:CONTAINS]->(url)
-        )
-        FOREACH (m IN e.user_mentions |
-          MERGE (mentioned:User {screen_name:m.screen_name})
-          ON CREATE SET mentioned.name = m.name
-          MERGE (tweet)-[:MENTIONS]->(mentioned)
-        )
-        FOREACH (r IN [r IN [t.in_reply_to_status_id] WHERE r IS NOT NULL] |
-          MERGE (reply_tweet:Tweet {id:r})
-          MERGE (tweet)-[:REPLY_TO]->(reply_tweet)
-        )
-        FOREACH (retweet_id IN [x IN [retweet.id] WHERE x IS NOT NULL] |
-            MERGE (retweet_tweet:Tweet {id:retweet_id})
-            MERGE (tweet)-[:RETWEETS]->(retweet_tweet)
-        )
-        """
-        
-        # Send Cypher query.
-        graph.cypher.execute(query, tweet=tweet)
-        
-        
-        print_green("Saving Tweet.")
-        if('text' in tweet):
+            query = """
+            UNWIND {tweet} AS t
+            WITH t
+            ORDER BY t.id
+            WITH t,
+                 t.entities AS e,
+                 t.user AS u,
+                 t.retweeted_status AS retweet
+            MERGE (tweet:Tweet {id:t.id})
+            SET tweet.text = t.text,
+                tweet.created_at = t.created_at,
+                tweet.favorites = t.favorite_count
+            MERGE (user:User {screen_name:u.screen_name})
+            SET user.name = u.name,
+                user.location = u.location,
+                user.followers = u.followers_count,
+                user.following = u.friends_count,
+                user.statuses = u.statusus_count,
+                user.profile_image_url = u.profile_image_url
+            MERGE (user)-[:POSTS]->(tweet)
+            MERGE (source:Source {name:t.source})
+            MERGE (tweet)-[:USING]->(source)
+            FOREACH (h IN e.hashtags |
+              MERGE (tag:Hashtag {name:LOWER(h.text)})
+              MERGE (tag)-[:TAGS]->(tweet)
+            )
+            FOREACH (u IN e.urls |
+              MERGE (url:Link {url:u.expanded_url})
+              MERGE (tweet)-[:CONTAINS]->(url)
+            )
+            FOREACH (m IN e.user_mentions |
+              MERGE (mentioned:User {screen_name:m.screen_name})
+              ON CREATE SET mentioned.name = m.name
+              MERGE (tweet)-[:MENTIONS]->(mentioned)
+            )
+            FOREACH (r IN [r IN [t.in_reply_to_status_id] WHERE r IS NOT NULL] |
+              MERGE (reply_tweet:Tweet {id:r})
+              MERGE (tweet)-[:REPLY_TO]->(reply_tweet)
+            )
+            FOREACH (retweet_id IN [x IN [retweet.id] WHERE x IS NOT NULL] |
+                MERGE (retweet_tweet:Tweet {id:retweet_id})
+                MERGE (tweet)-[:RETWEETS]->(retweet_tweet)
+            )
+            """
+             # Send Cypher query.
+            graph.cypher.execute(query, tweet=tweet)
+            
+            
+            print_green("Saving Tweet.")
             print " %s" % tweet['text']
         
-        return(True)
+            return(True)
+        else:
+            print_red("Tweet doesn't contain data needed")
+        
+       
             
         
     def on_error(self, status):
@@ -102,13 +109,23 @@ class listener(StreamListener):
 auth = OAuthHandler(ConsumerKey, ConsumerSecret)
 auth.set_access_token(AccessToken, AccessSecret)
 
-#Handle exit
-try:
-    twitterStream = Stream(auth, listener())
+while True:
+    try:
+        twitterStream = Stream(auth, listener())
+        twitterStream.filter(track=[Filter], follow=[UserIds])
+        
+    except IncompleteRead:
+        print_red("IncompleteRead caught. Waiting......")
+        continue
+    except ProtocolError:
+        print_red("Protocol Error caught. Waiting......")
+        time.sleep(2)
+    except (KeyboardInterrupt,SystemExit):
+        stream.disconnect()
+        print "Exiting..."
+        break
 
-    twitterStream.filter(track=[Filter], follow=[UserIds])
-except (KeyboardInterrupt, SystemExit):
-    print "Exiting..."
+
 
 
 
