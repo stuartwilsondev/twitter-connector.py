22702b52672e3edb3be9bff2a05c1ba3ac42bbb0
Stuart Wilson <stuart@stuartwilsondev.com>
2015-10-03 15:11:32 +0100

    Typo

--- twitter-neo4j.py
+++ twitter-neo4j.py
@@ -43,7 +43,7 @@ def print_red(text): print ("\033[91m {}\033[00m" .format(text))
 #Print Green
 def print_green(text): print ("\033[92m {}\033[00m" .format(text))
 
-#PRint Yellow
+#Print Yellow
 def print_yellow(text): print ("\033[93m {}\033[00m" .format(text))
 
 class listener(StreamListener):
